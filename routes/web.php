42fbd0dcb45519fe34e66191d42cd27c4ce2d247
josdinhson2003 <son28gt@gmail.com>
2026-03-11 22:57:31 +0700

    commit parishioner management and school year copy feature

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,7 @@ Auth::routes();
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', Home::class)->name('dashboard');
 
-    Route::get('/parishioners', ParishionersManager::class)
+    Route::get('/parishioner', ParishionersManager::class)
         ->name('parishioners.index');
 
     Route::middleware('role:parish_admin|catechist')->group(function () {
@@ -103,19 +103,6 @@ Route::middleware('auth')->group(function () {
         Route::get('/classes/{id}/catechists', AssignTeacher::class)
             ->name('classes.catechists');
 
-        // Route::get('/students/create', StudentEdit::class)
-        //     ->name('students.create');
-
-        // Route::get('/studentss/import', StudentImportPreview::class)
-        //     ->name('students.import');
-
-        // Route::get('/students/{id}/edit', StudentEdit::class)
-        //     ->name('students.edit');
-
-        // // Download template Excel
-        // Route::get('/studentss/import/template', [StudentImportController::class, 'template'])
-        //     ->name('students.import.template');
-
         Route::get('/students/create', StudentEdit::class)
             ->name('students.create');
 
@@ -127,33 +114,6 @@ Route::middleware('auth')->group(function () {
 
         Route::get('/students/{id}/edit', StudentEdit::class)
             ->name('students.edit');
-
-        // Route::get('/session', SessionManager::class)
-        //     ->name('session.index');
-
-        // Route::get('/scores', ScoreManager::class)
-        //     ->name('scores.index');
-
-        // Route::get('/school-years', NamHocManager::class)
-        //     ->name('school-years.index');
-
-        // Route::get('/grades', BlockManager::class)
-        //     ->name('grades.index');
-
-        // Route::get('/catechists', TeacherManager::class)
-        //     ->name('catechists.index');
-
-        // Route::get('/catechists/import', TeacherImportPreview::class)
-        //     ->name('catechists.import');
-
-        // Route::get('/parish-children', ParishChild::class)
-        //     ->name('parish-children.index');
-
-        // Route::get('/holy-names', HolyManager::class)
-        //     ->name('holy-names.index');
-
-        // Route::get('/parishioners', ParishionersManager::class)
-        //     ->name('parishioners.index');
 
         Route::get('/catechists/import', TeacherImportPreview::class)
             ->name('catechists.import');
@@ -171,11 +131,8 @@ Route::middleware('auth')->group(function () {
         Route::get('/school-years', NamHocManager::class)
             ->name('school-years.index');
 
-            Route::get('/school-years/copy', CopyNamHoc::class)
-                ->name('school-years.copy');
-
-        // Route::get('/grades', BlockManager::class)
-        //     ->name('grades.index');
+        Route::get('/school-years/copy', CopyNamHoc::class)
+            ->name('school-years.copy');
 
         Route::get('/holy-names', HolyManager::class)
             ->name('holy-names.index');
