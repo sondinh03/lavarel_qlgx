64150e92893a817eb3773a0b59f651e5d688daab
josdinhson2003 <son28gt@gmail.com>
2026-04-17 21:18:57 +0700

    commit avatar update

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,8 +58,8 @@ return [
         // ckfinder
         'uploads' => [
             'driver' => 'local',
-            'root' => storage_path('app/uploads'),
-            'url' => env('APP_URL'),
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL') . '/uploads',
             'visibility' => 'public',
         ],
 
